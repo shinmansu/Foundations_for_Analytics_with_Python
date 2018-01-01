3cf65e39b08ef3e579fe8a6fc4da29a56f0d0eb7
shinmansu <sksms4687@naver.com>
2018-01-01 23:21:24 +0900

    오타 수정

--- first_script_write.py
+++ first_script_write.py
@@ -14,8 +14,8 @@ output_file = sys.argv[1]
 filewriter = open(output_file, 'w')
 for index_value in range(len(my_letters)):
     if index_value < (max_index-1):
-        filewriter.wrtie(my_letters[index_value]+'\t')
+        filewriter.write(my_letters[index_value]+'\t')
     else:
         filewriter.write(my_letters[index_value]+'\n')
-        filewrtier.close()
+        filewriter.close()
 print("Output #142: Ouput written to file")
