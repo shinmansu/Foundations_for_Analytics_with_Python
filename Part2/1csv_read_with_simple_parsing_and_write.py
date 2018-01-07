6cd18df85e49e0313b157b41b230ba1d2fe760c1
shinmansu <sksms4687@naver.com>
2018-01-08 00:55:29 +0900

    2.5

--- Part2/1csv_read_with_simple_parsing_and_write.py
+++ Part2/1csv_read_with_simple_parsing_and_write.py
@@ -1,18 +1,14 @@
-#!/usr/bin/env python3
 import sys
 
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 
-with open(input_file, 'r', newline='')as filereader :
-    with open(output_file, 'w', newline='')as filewriter :
-          header = filereader.readline()
-          header = header.strip()
-          header_list = header.split(',')
-          print(header_list)
-          filewriter.wrtie(','.join(map(str,header_list)+'\n'))
-          for row in filereader:
-              row = row.strip()
-              row_list = row.split(',')
-              print(row_list)
-              filewriter.write(','.jon(map(str,row_list))+'\n')
+with open(input_file, 'r', newline='') as filereader:
+    with open(output_file, 'w', newline='') as filewriter:
+        header = filereader.readline()
+        header_list = header.strip().split(',')
+        filewriter.write(','.join(map(str, header_list))+'\n')
+        for row in filereader:
+            row = row.strip()
+            row_list = row.split(',')
+            filewriter.write(','.join(map(str, row_list))+'\n')
--- Part2/pandas_parsing_and_write.py
+++ Part2/pandas_parsing_and_write.py
@@ -1,4 +1,3 @@
-# !/usr/bin/env python3
 import sys
 import pandas as pd
 
@@ -6,5 +5,4 @@ input_file = sys.argv[1]
 output_file = sys.argv[2]
 
 data_frame = pd.read_csv(input_file)
-print(data_frame)
-data_frame.to_csv(output_file, index = False)
+data_frame.to_csv(output_file, index=False)
