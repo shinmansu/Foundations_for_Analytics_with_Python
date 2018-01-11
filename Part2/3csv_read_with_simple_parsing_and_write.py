8d3d947ba89f8579406f9d65479628cc35754d99
shinmansu <sksms4687@naver.com>
2018-01-12 08:17:18 +0900

    2장 수정

--- Part2/3csv_read_with_simple_parsing_and_write.py
+++ Part2/3csv_read_with_simple_parsing_and_write.py
@@ -8,5 +8,10 @@ with open(input_file, 'r', newline='') as csv_in_file:
     with open(output_file, 'w', newline='') as csv_out_file:
         filereader = csv.reader(csv_in_file, delimiter=',')
         filewriter = csv.writer(csv_out_file, delimiter=',')
+        header = next(filereader)
+        filewriter.writerow(header)
         for row_list in filereader:
-            filewriter.writerow(row_list)
+            supplier = str(row_list[0]).strip()
+            cost = str(row_list[3]).strip('$').replace(',', '')
+            if supplier == 'Supplier Z' or float(cost) > 600.0:
+                filewriter.writerow(row_list)
